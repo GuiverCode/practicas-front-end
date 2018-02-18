43ca48f7f593558f7457017182ac9089d7fd5efa
Guillermo Veron <35076289+GuiverCode@users.noreply.github.com>
2018-02-18 16:04:27 -0300

    Update basic-function.js

--- Javascript/cap03-function, methods and objects/js/basic-function.js
+++ Javascript/cap03-function, methods and objects/js/basic-function.js
@@ -1,8 +1,10 @@
 var msg = 'Sign up to recieve our newsletter for 10% off!';
 
+//bloque de funcion javascript
 function updateMessage(){
     var elMessage = document.getElementById('message');
     elMessage.textContent = msg;
 }
 
+//llamada a la funcion
 updateMessage();
